e1fdbcc883d57911c6a728273f4da278e9852b76
John Dunbar <u08jd21@abdn.ac.uk>
2022-05-11 20:25:22 +0100

    Challenge_1 not functional. Use Lux-ferre_Solution

--- Python/Event_1/Challenge_1.py
+++ Python/Event_1/Challenge_1.py
@@ -1,1 +1,16 @@
 # Challenge 1 for the Code Kata.
+
+def wordwrap(string, num):
+    """..."""
+    arr = string.split(' ')
+    n = ''
+    for item in arr:
+        while len(item) <= num:
+            n += item
+
+    
+
+    return n
+
+
+print(wordwrap('The quick brown fox jumps over the lazy dog', 15))
